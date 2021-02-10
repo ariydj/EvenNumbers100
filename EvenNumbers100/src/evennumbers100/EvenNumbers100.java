ddf16ef95d92c703e5d70e6520f56c182529d654
David Ariyaratnam <David Ariyaratnam@DESKTOP-7HFC4NF.home>
2021-02-10 12:09:15 -0500

    Even Numbers On New Line Until 100

---
+++ EvenNumbers100/src/evennumbers100/EvenNumbers100.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evennumbers100;
+
+/**
+ *
+ * @author David Ariyaratnam
+ */
+public class EvenNumbers100 {
+
+    public static void main(String[] args) {
+       for (int i = 0; i <= 100; i += 2)
+           System.out.print(i + "\n");
+    }
+    
+    
+}
